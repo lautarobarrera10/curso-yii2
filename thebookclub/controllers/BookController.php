6df0192fc6032cb5c1919b6536b6b0cb3cf79013
lautarobarrera10 <lautarobarrera12@gmail.com>
2024-08-25 14:42:47 -0300

    Web Controllers

---
+++ thebookclub/controllers/BookController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use app\models\Book;
+
+class BookController extends Controller {
+    public function actionAll(){
+        $book = Book::find()->all();
+        return serialize($book);
+    }
+
+    public function actionDetail($id){
+        $book = Book::findOne($id);
+        if (empty($book)){
+            // todo error
+        }
+        return $book->title;
+    }
+}
